e475ca3f056c171d030c5562b7a4e87b0955e4d6
iofq <cjriddz@protonmail.com>
2022-11-26 21:17:29 -0600

    fix typo

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -2,7 +2,9 @@ resource "oci_core_instance" "vm_instance_ampere" {
     availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[0].name
     compartment_id                      = oci_identity_compartment.tf-compartment.id
     shape                               = "VM.Standard.A1.Flex"
-    display_name                        = join("", [var.vm_name_template, "-arm"]) is_pv_encryption_in_transit_enabled = true preserve_boot_volume = false
+    display_name                        = join("", [var.vm_name_template, "-arm"]) 
+    is_pv_encryption_in_transit_enabled = true 
+    preserve_boot_volume = false
 
     shape_config {
         memory_in_gbs = 24
@@ -39,7 +41,7 @@ resource "oci_core_instance" "vm_instance_ampere" {
         inline = [
             "echo 'This instance was provisioned by Terraform in ${var.region}.' | sudo tee /etc/motd",
             "sudo apt update && sudo apt install ansible git",
-            "ansible-pull -C ansible -U https://github.com/lab-xyz/lab.git ansible/site.yml"
+            "ansible-pull -C ansible -U https://github.com/lab-xyz/lab.git ansible/site.yml",
         ]
     }
 }
