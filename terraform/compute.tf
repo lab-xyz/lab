d085370f886c360d45e0142051c819dc416e5050
iofq <cjriddz@protonmail.com>
2022-11-27 13:02:09 -0600

    init terraform config (#1)

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,9 +1,16 @@
+resource "random_id" "id" {
+    byte_length = 2
+    keepers = {
+        uuid = uuid()
+    }
+}
+
 resource "oci_core_instance" "vm_instance_ampere" {
     availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[0].name
     compartment_id                      = oci_identity_compartment.tf-compartment.id
     shape                               = "VM.Standard.A1.Flex"
-    display_name                        = join("", [var.vm_name_template, "-arm"])
-    is_pv_encryption_in_transit_enabled = true
+    display_name                        = join("", [var.vm_name_template, "-arm-", random_id.id.hex]) 
+    is_pv_encryption_in_transit_enabled = true 
     preserve_boot_volume = false
 
     shape_config {
@@ -25,7 +32,7 @@ resource "oci_core_instance" "vm_instance_ampere" {
         assign_public_ip          = true
         subnet_id                 = oci_core_subnet.homelab_subnet.id
         assign_private_dns_record = true
-        hostname_label            = join("", [var.vm_name_template, "-arm"])
+        hostname_label            = join("", [var.vm_name_template, "-arm", random_id.id.hex])
         private_ip                = join(".", ["10", "0", "0", 110])
         nsg_ids                   = [oci_core_network_security_group.homelab_nsg.id]
     }
@@ -39,7 +46,11 @@ resource "oci_core_instance" "vm_instance_ampere" {
 
     provisioner "remote-exec" {
         inline = [
-            "echo 'This instance was provisioned by Terraform in ${var.region}.' | sudo tee /etc/motd",
+            "echo 'This instance was provisioned by Terraform in Oracle Cloud region ${var.region}.' | sudo tee /etc/motd",
+            "sudo apt update",
+            "sudo apt update",
+            "sudo apt install -y ansible git",
+            "ansible-pull -U https://github.com/lab-xyz/lab.git -e k3s_master_ip=${var.k3s_master_ip} -e k3s_token=${var.k3s_token} -e k3s_public_ip=${self.public_ip} ansible/site.yml",
         ]
     }
 }
