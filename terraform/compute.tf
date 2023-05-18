6712aae7ee2193584a3a471708c6500bb7069b70
iofq <mail@10110110.xyz>
2023-05-18 06:15:05 -0400

    tf changes

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,15 +1,15 @@
 resource "oci_core_instance" "vm_instance_ampere" {
-    count = 1
-    availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[2].name
+    count = 2
+    availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[1].name
     compartment_id                      = oci_identity_compartment.tf-compartment.id
     shape                               = "VM.Standard.A1.Flex"
-    display_name                        = join("", [var.vm_name_template, "-arm"]) 
+    display_name                        = join("", [var.vm_name_template, "-arm", count.index]) 
     is_pv_encryption_in_transit_enabled = true 
     preserve_boot_volume = false
 
     shape_config {
-        memory_in_gbs = 24
-        ocpus         = 4
+        memory_in_gbs = 12
+        ocpus         = 2
     }
 
     metadata = {
@@ -26,13 +26,13 @@ resource "oci_core_instance" "vm_instance_ampere" {
         assign_public_ip          = true
         subnet_id                 = oci_core_subnet.homelab_subnet.id
         assign_private_dns_record = true
-        hostname_label            = join("", [var.vm_name_template, "-arm"])
+        hostname_label            = join("", [var.vm_name_template, "-arm", count.index])
         nsg_ids                   = [oci_core_network_security_group.homelab_nsg.id]
     }
 
     connection {
         type        = "ssh"
-        host        = "${oci_core_instance.vm_instance_ampere[0].public_ip}"
+        host        = "${self.public_ip}"
         user        = "ubuntu"
         private_key = "${var.ssh_private_key}"
     }
@@ -43,53 +43,7 @@ resource "oci_core_instance" "vm_instance_ampere" {
             "sudo apt update",
             "sudo apt update",
             "sudo apt install -y ansible git",
-            "ansible-pull -U https://github.com/lab-xyz/lab.git -e k3s_master_ip=${var.k3s_master_ip} -e k3s_token=${var.k3s_token} -e k3s_public_ip=${oci_core_instance.vm_instance_ampere[0].public_ip} ansible/site.yml",
-        ]
-    }
-
-}
-
-resource "oci_core_instance" "vm_instance_x86_64" {
-    count = 1
-    availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[2].name
-    compartment_id                      = oci_identity_compartment.tf-compartment.id
-    shape                               = "VM.Standard.E2.1.Micro"
-    display_name                        = join("", [var.vm_name_template, "-amd64"]) 
-    is_pv_encryption_in_transit_enabled = true 
-    preserve_boot_volume = false
-
-    metadata = {
-        ssh_authorized_keys = var.ssh_public_key
-    }
-
-    source_details {
-        source_id   = var.vm_image_amd64
-        source_type = "image"
-        boot_volume_size_in_gbs = 100
-    }
-
-    create_vnic_details {
-        assign_public_ip          = true
-        subnet_id                 = oci_core_subnet.homelab_subnet.id
-        assign_private_dns_record = true
-        hostname_label            = join("", [var.vm_name_template, "-amd64"])
-        nsg_ids                   = [oci_core_network_security_group.homelab_nsg.id]
-    }
-
-    connection {
-        type        = "ssh"
-        host        = "${oci_core_instance.vm_instance_x86_64[0].public_ip}"
-        user        = "ubuntu"
-        private_key = "${var.ssh_private_key}"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "echo 'This instance was provisioned by Terraform in Oracle Cloud region ${var.region}.' | sudo tee /etc/motd",
-            "sudo apt update",
-            "sudo apt update",
-            "sudo apt install -y ansible git",
-            "ansible-pull -U https://github.com/lab-xyz/lab.git -e k3s_master_ip=${var.k3s_master_ip} -e k3s_token=${var.k3s_token} -e k3s_public_ip=${oci_core_instance.vm_instance_x86_64[0].public_ip} ansible/site.yml",
+            "ansible-pull -U https://github.com/lab-xyz/lab.git -e k3s_master_ip=${var.k3s_master_ip} -e k3s_token=${var.k3s_token} -e k3s_public_ip=${self.public_ip} ansible/site.yml",
         ]
     }
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "arm_public_ip" {
+output "arm_public_ip0" {
   value = oci_core_instance.vm_instance_ampere[0].public_ip
 }
 
-output "x86_64_public_ip" {
-  value = oci_core_instance.vm_instance_x86_64[0].public_ip
+output "arm_public_ip1" {
+  value = oci_core_instance.vm_instance_ampere[1].public_ip
 }
