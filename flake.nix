72c2a5b131e36ff7ea7380836e3a9445ffeb0547
iofq <cjriddz@protonmail.com>
2023-05-19 04:04:19 -0500

    add gitea pg db, flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "a dev env for lab-xyz repo";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ 
+        fluxcd
+        kubectl
+        postgresql
+      ];
+    };
+  };
+}
