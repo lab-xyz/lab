08d41de346e55832dfbf9748c61b14c00a18fddf
iofq <cjriddz@protonmail.com>
2024-03-17 17:08:25 -0500

    spin up one big oc node

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,5 +1,5 @@
 resource "oci_core_instance" "vm_instance_ampere" {
-    count = 2
+    count = 1
     availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[0].name
     compartment_id                      = oci_identity_compartment.tf-compartment.id
     shape                               = "VM.Standard.A1.Flex"
@@ -8,8 +8,8 @@ resource "oci_core_instance" "vm_instance_ampere" {
     preserve_boot_volume = false
 
     shape_config {
-        memory_in_gbs = 12
-        ocpus         = 2
+        memory_in_gbs = 16
+        ocpus         = 4
     }
 
     metadata = {
@@ -19,7 +19,7 @@ resource "oci_core_instance" "vm_instance_ampere" {
     source_details {
         source_id   = var.vm_image_arm
         source_type = "image"
-        boot_volume_size_in_gbs = 100
+        boot_volume_size_in_gbs = 200
     }
 
     create_vnic_details {
@@ -36,15 +36,4 @@ resource "oci_core_instance" "vm_instance_ampere" {
         user        = "ubuntu"
         private_key = "${var.ssh_private_key}"
     }
-
-    provisioner "remote-exec" {
-        inline = [
-            "echo 'This instance was provisioned by Terraform in Oracle Cloud region ${var.region}.' | sudo tee /etc/motd",
-            "sudo apt update",
-            "sudo apt update",
-            "sudo apt install -y ansible git",
-            "ansible-pull -U https://github.com/lab-xyz/lab.git -e k3s_master_ip=${var.k3s_master_ip} -e k3s_token=${var.k3s_token} -e k3s_public_ip=${self.public_ip} ansible/site.yml",
-        ]
-    }
-
 }
