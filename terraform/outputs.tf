a8728241663d920472124061c14ed3d680fc4564
iofq <cjriddz@protonmail.com>
2024-03-17 17:10:45 -0500

    remove bad tf output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "arm_public_ip0" {
   value = oci_core_instance.vm_instance_ampere[0].public_ip
 }
-
-output "arm_public_ip1" {
-  value = oci_core_instance.vm_instance_ampere[1].public_ip
-}
