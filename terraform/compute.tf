6fef9dae208e73d6cbf602656551bdc8812d3725
iofq <cjriddz@protonmail.com>
2024-03-17 18:46:44 -0500

    add x64 vm

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -19,7 +19,7 @@ resource "oci_core_instance" "vm_instance_ampere" {
     source_details {
         source_id   = var.vm_image_arm
         source_type = "image"
-        boot_volume_size_in_gbs = 200
+        boot_volume_size_in_gbs = 150
     }
 
     create_vnic_details {
@@ -29,11 +29,31 @@ resource "oci_core_instance" "vm_instance_ampere" {
         hostname_label            = join("", [var.vm_name_template, "-arm", count.index])
         nsg_ids                   = [oci_core_network_security_group.homelab_nsg.id]
     }
+}
 
-    connection {
-        type        = "ssh"
-        host        = "${self.public_ip}"
-        user        = "ubuntu"
-        private_key = "${var.ssh_private_key}"
-    }
+resource "oci_core_instance" "vm_instance_x86_64" {
+  count                               = 1
+  availability_domain                 = data.oci_identity_availability_domains.ads.availability_domains[2].name
+  compartment_id                      = oci_identity_compartment.tf-compartment.id
+  shape                               = "VM.Standard.E2.1.Micro"
+  display_name                        = join("", [var.vm_name_template, "-x64", count.index]) 
+  is_pv_encryption_in_transit_enabled = true
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+
+  source_details {
+    source_id   = var.vm_image_ocid_x86_64
+    source_type = "image"
+    boot_volume_size_in_gbs = 50
+  }
+
+  create_vnic_details {
+      assign_public_ip          = true
+      subnet_id                 = oci_core_subnet.homelab_subnet.id
+      assign_private_dns_record = true
+      hostname_label            = join("", [var.vm_name_template, "-x84", count.index])
+      nsg_ids                   = [oci_core_network_security_group.homelab_nsg.id]
+  }
 }
